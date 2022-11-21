ad90b29617ca20838f4c6f34ecaa5b991da1a6d7
guorong <dudurong009@gmail.com>
2022-11-21 21:33:32 +0800

    Add the "IsZeroConfig" & "IsZeroChip"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use halo2_proofs::{
+    arithmetic::FieldExt,
+    plonk::{Advice, Column, Expression},
+};
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Clone)]
+struct IsZeroConfig<F> {
+    pub value_inv: Column<Advice>,
+    pub is_zero_expr: Expression<F>,
+}
+
+pub struct IsZeroChip<F: FieldExt> {
+    config: IsZeroConfig<F>,
 }
