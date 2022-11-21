b34491387d8cc9407fba1193e57c64f85bb91883
guorong <dudurong009@gmail.com>
2022-11-21 22:39:06 +0800

    Add the "CustomConfig" & "CustomChip"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 pub mod is_zero_gadget;
+
+use halo2_proofs::{
+    arithmetic::FieldExt,
+    plonk::{Advice, Column, Selector},
+};
 use is_zero_gadget::*;
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Clone)]
+pub struct CustomConfig<F> {
+    s: Selector,
+    a: Column<Advice>,
+    b: Column<Advice>,
+    c: Column<Advice>,
+    a_equals_b: IsZeroConfig<F>,
+    output: Column<Advice>,
+}
+
+#[derive(Debug)]
+pub struct CustomChip<F> {
+    config: CustomConfig<F>,
+}
+
+impl<F: FieldExt> CustomChip<F> {
+    pub fn construct(config: CustomConfig<F>) -> Self {
+        CustomChip { config }
+    }
 }
